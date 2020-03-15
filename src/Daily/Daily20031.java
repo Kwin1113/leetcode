2e608e0aac3ef4f191b88ca2a48d8df3ed9d3f8b
kwin1113 <969263948@qq.com>
2020-03-15 22:52:57 +0800

    695. 岛屿的最大面积

---
+++ src/Daily/Daily20031.java
@@ -0,0 +1,90 @@
+package Daily;
+
+/**
+ * @author Kwin
+ * @since 2020/3/15 21:27
+ * <p>
+ * 695. 岛屿的最大面积
+ * <p>
+ * 给定一个包含了一些 0 和 1的非空二维数组 grid , 一个 岛屿 是由四个方向 (水平或垂直) 的 1 (代表土地) 构成的组合。你可以假设二维矩阵的四个边缘都被水包围着。
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0。)
+ * <p>
+ * 示例 1:
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是11，因为岛屿只能包含水平或垂直的四个方向的‘1’。
+ * <p>
+ * 示例 2:
+ * [[0,0,0,0,0,0,0,0]]
+ * 对于上面这个给定的矩阵, 返回 0。
+ * <p>
+ * 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+ */
+public class Daily20031 {
+
+    static int[][] grid1 = new int[][]{
+            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+    };
+
+    static int[][] grid2 = new int[][]{
+            {0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    static int[][] grid3 = new int[][]{
+            {1, 1, 0, 0, 0},
+            {1, 1, 0, 0, 0},
+            {0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 1}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(solution1(grid1));
+        System.out.println(solution1(grid2));
+        System.out.println(solution1(grid3));
+    }
+
+    static int solution1(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int check = check(grid, i, j);
+                    max = Math.max(check, max);
+                }
+            }
+        }
+        return max;
+    }
+
+    // 检查坐标为(x, y)的点四周有多少块陆地
+    static int check(int[][] grid, int x, int y) {
+        // 如果遇到了墙壁，直接返回0
+        if (!(0 <= x && x < grid.length
+                && 0 <= y && y < grid[0].length)) {
+            return 0;
+        }
+        if (grid[x][y] != 1) {
+            return 0;
+        }
+        // 题解提示
+        grid[x][y] = 0;
+        return 1 + check(grid, x - 1, y) + check(grid, x + 1, y) + check(grid, x, y - 1) + check(grid, x, y + 1);
+    }
+
+    static int bestSolution(int[][] grid) {
+        return 0;
+    }
+}
