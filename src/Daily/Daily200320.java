676a1fe82d45434d4bedf2bf63b4ba409d966ae8
kwin1113 <969263948@qq.com>
2020-03-20 23:15:50 +0800

    面试题40. 最小的k个数

---
+++ src/Daily/Daily200320.java
@@ -0,0 +1,51 @@
+package Daily;
+
+import java.util.Arrays;
+
+/**
+ * @author Kwin
+ * @since 2020/3/20 21:51
+ *
+ * 面试题40. 最小的k个数
+ *
+ * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ *
+ * 示例 1：
+ * 输入：arr = [3,2,1], k = 2
+ * 输出：[1,2] 或者 [2,1]
+ *
+ * 示例 2：
+ * 输入：arr = [0,1,2,1], k = 1
+ * 输出：[0]
+ *
+ * 限制：
+ * 0 <= k <= arr.length <= 10000
+ * 0 <= arr[i] <= 10000
+ */
+public class Daily200320 {
+
+    static int[] arr1 = new int[]{3, 2, 1};
+    static int k1 = 2;
+
+    static int[] arr2 = new int[]{0, 1, 2, 1};
+    static int k2 = 1;
+
+    static int[] arr3 = new int[]{4, 5, 1, 6, 2, 7, 3, 8};
+    static int k3 = 4;
+
+    public static void main(String[] args) {
+//        int[] res = solution1(arr1, k1);
+//        int[] res = solution1(arr2, k2);
+        int[] res = solution1(arr3, k3);
+        for (int i : res) {
+            System.out.println(i);
+        }
+    }
+
+    static int[] solution1(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] result = new int[k];
+        System.arraycopy(arr, 0, result, 0, k);
+        return result;
+    }
+}
