906a0e7a5f07facba29064ccc7fe0c0fbb3321a9
kwin1113 <969263948@qq.com>
2020-04-06 22:40:51 +0800

    72. 编辑距离

---
+++ src/Daily/Daily200406.java
@@ -0,0 +1,68 @@
+package Daily;
+
+/**
+ * @author Kwin
+ * @since 2020/4/6 21:29
+ * <p>
+ * 72. 编辑距离
+ * <p>
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * <p>
+ * 示例 1：
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * <p>
+ * 示例 2：
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ */
+public class Daily200406 {
+
+    static String word11 = "horse";
+    static String word12 = "ros";
+
+    static String word21 = "intention";
+    static String word22 = "execution";
+
+    public static void main(String[] args) {
+        System.out.println(bestSolution(word11, word12));
+        System.out.println(bestSolution(word21, word22));
+    }
+
+    static int bestSolution(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= len2; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
+}
